e80b81a335c97846806d4d110a6a86f0dd09f6e3
신강식 <chickang9@gmail.com>
2020-05-25 22:44:57 +0900

    ADD Panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,68 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ RoomAdmin Admin Definitions """
 
-    pass
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "host__gender",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "city", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")},),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")},),
+        ("Last Details", {"fields": ("host",)},),
+    )
+
+    ordering = (
+        "name",
+        "price",
+    )
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "Poatao"
+
+    count_amenities.short_description = "Hello Sexy!"
 
 
 @admin.register(models.Photo)
