a6ad4662246a0c4256ba1a1326cdc9ab124bd907
신강식 <chickang9@gmail.com>
2020-06-02 21:43:09 +0900

    Add lecture 7

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,7 +7,13 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definitions """
 
-    pass
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Room)
@@ -28,6 +34,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
@@ -75,8 +82,13 @@ class RoomAdmin(admin.ModelAdmin):
     def count_amenities(self, obj):
         return obj.amenities.count()
 
+    def count_photos(self, obj):
+        return obj.photos.count()
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
     """ PhotoAdmin Admin Definitions """
+
+    pass
