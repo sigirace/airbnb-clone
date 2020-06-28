a7fa72e94cf6cf4fed32d905236f171925074351
신강식 <chickang9@gmail.com>
2020-06-28 16:26:36 +0900

    Add HomeView

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from . import models
+
 
 # Create your views here.
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
